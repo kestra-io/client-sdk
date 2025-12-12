0d7077fa6ca9696f36f09d2259dc52bdfc65017d
Bart Ledoux <bledoux@kestra.io>
2025-12-12 18:40:36 +0100

    add comment and todo

--- javascript/javascript-sdk/src/model/IQueryFilter.d.ts
+++ javascript/javascript-sdk/src/model/IQueryFilter.d.ts
@@ -1,6 +1,10 @@
 import { QueryFilterOpStatic } from './QueryFilterOp.js'
 
+/**
+ * Pass this model to represent filter queries
+ */
 export interface IQueryFilter {
+    // TODO: help users select the right property on each table by restricting this by table
     property: string;
     operation: typeof QueryFilterOpStatic[keyof typeof QueryFilterOpStatic];
     value: any;
