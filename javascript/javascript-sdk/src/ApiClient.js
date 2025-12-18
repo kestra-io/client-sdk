19e2ba3184c13eec522de34383fd6a947c6bd431
Bart Ledoux <bledoux@kestra.io>
2025-12-18 21:51:18 +0100

    ci: version js 1.0.9

--- javascript/javascript-sdk/src/ApiClient.js
+++ javascript/javascript-sdk/src/ApiClient.js
@@ -54,7 +54,7 @@ class ApiClient {
          * @default {}
          */
         this.defaultHeaders = {
-            'User-Agent': 'OpenAPI-Generator/1.0.8/Javascript'
+            'User-Agent': 'OpenAPI-Generator/1.0.9/Javascript'
         };
 
         /**
