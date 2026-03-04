9e88fb6277576bc728e7d0934ce554d9fcf70ca1
steven meek <smeek@kestra.io>
2026-03-04 09:48:02 +0000

    chore: fix version number.

--- python/python-sdk/kestrapy/configuration.py
+++ python/python-sdk/kestrapy/configuration.py
@@ -533,7 +533,7 @@ conf = kestrapy.Configuration(
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 1.4.0-SNAPSHOT\n"\
+               "Version of the API: 1.3.1\n"\
                "SDK Package Version: 1.0.10".\
                format(env=sys.platform, pyversion=sys.version)
 
