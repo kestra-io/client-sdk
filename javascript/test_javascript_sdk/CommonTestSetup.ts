167580c346e3a92dafbbeb313eaaf07eb1dc616d
Bart Ledoux <bledoux@kestra.io>
2026-06-03 10:50:27 +0200

    test: add setup logic to initialize tenant in CommonTestSetup

--- javascript/test_javascript_sdk/CommonTestSetup.ts
+++ javascript/test_javascript_sdk/CommonTestSetup.ts
@@ -97,6 +97,25 @@ beforeAll(async () => {
             return Promise.reject(error);
         });
     }
+    const config = await fetch(`${baseURL}/api/v1/setup`);
+    if (config.status === 200 && (!(await config.json())?.done)) {
+        console.log("Running setup...");
+        const res = await fetch(`${baseURL}/api/v1/setup`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username,
+                password,
+                tenant: {
+                    id: tenantId,
+                    name: tenantId,
+                }
+            }),
+        })
+        console.log("Setup response:", res.status, res.statusText);
+    }
     setSelectedTenant(tenantId);
 });
 
