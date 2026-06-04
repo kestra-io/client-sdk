197ecce47513407b9ecdb97e6b02c67055a34c64
Bart Ledoux <bledoux@kestra.io>
2026-06-04 16:22:07 +0200

    refactor: enhance setup function with retry logic and status check

--- javascript/test_javascript_sdk/setupTests.ts
+++ javascript/test_javascript_sdk/setupTests.ts
@@ -1,30 +1,46 @@
+import { beforeAll } from "vitest";
 import fixtures from "./fixtures.json" with { type: "json" };
 
 const { baseURL, username, password, tenantId } = fixtures;
 
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
 async function setup() {
-    const config = await fetch(`${baseURL}/api/v1/setup`);
-    if (config.status !== 200 || (!(await config.json())?.done)) {
-        console.log("Running setup...");
-        const res = await fetch(`${baseURL}/api/v1/setup`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                username,
-                password,
-                tenant: {
-                    id: tenantId,
-                    name: tenantId,
-                }
-            }),
-        })
-        console.log("Setup response:", res.status, res.statusText);
+
+    for (let attempt = 1; attempt <= 20; attempt++) {
+        try {
+            const configResponse = await fetch(`${baseURL}/api/v1/setup`);
+
+            if (configResponse.status !== 200) {
+                console.log("Setup already completed.");
+                return;
+            }
+            break; // If we got a 200, break out of the loop and run setup
+        } catch (error) {
+            console.error("Error checking setup status");
+            await sleep(500); // Wait for 2 seconds before retrying
+            continue;
+        }
     }
+
+    console.log("Running setup...");
+    const res = await fetch(`${baseURL}/api/v1/setup`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username,
+            password,
+            tenant: {
+                id: tenantId,
+                name: tenantId,
+            }
+        }),
+    })
+    console.log("Setup response:", res.status, res.statusText);
 }
 
-setup().catch((error) => {
-    console.error("Error during setup:", error);
-    process.exit(1);
+beforeAll(async () => {
+    await setup();
 });
